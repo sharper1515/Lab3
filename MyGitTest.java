934f6232cb34838488f40845bc56d2aa47d76525
Steve Harper <sharper@carroll.edu>
2018-10-06 13:44:20 -0600

    Changed message in branch A

--- MyGitTest.java
+++ MyGitTest.java
@@ -2,8 +2,6 @@
 public class MyGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Steve says Hello Git Test");// TODO Auto-generated method stub
-
-	}
+		System.out.println("Steve says Hello Git Test Branch AAAA");}
 
 }
