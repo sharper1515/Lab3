88fa0bafd0d04b3c2a474b7feafef94145a3712b
Steve Harper <sharper@carroll.edu>
2018-10-06 16:38:41 -0600

    chaned main message

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,7 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		System.out.println
-		("Steve Hello Git Test in CLOUD");}
+		("No my name isSteve Hello Git Test in CLOUD");}
 
 
 }
