506be6b425a8781105cee53efdf7a3c90a7aba24
Steve Harper <sharper@carroll.edu>
2018-10-07 13:04:02 -0600

    A

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,7 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		System.out.println
-		("No my name isSteve Hello Git Test in CLOUD");}
+		("A No my name isSteve Hello Git Test in CLOUD");}
 
 
 }
