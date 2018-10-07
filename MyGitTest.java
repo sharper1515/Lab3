fc1342d8ca12499efad21f23596c87b3fb55517d
sharper1515 <sharper@carroll.edu>
2018-10-07 13:50:41 -0600

    Update MyGitTest.java

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,7 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		System.out.println
-		("C No my name isSteve Hello Git Test in CLOUD");}
+		("x No my name isSteve Hello Git Test in CLOUD");}
 
 
 }
