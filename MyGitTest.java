956c02cfd5a2ba8d25be9a03a9294b77c7e8a244
Steve Harper <sharper@carroll.edu>
2018-10-11 12:32:04 -0600

    test

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,9 +3,5 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		System.out.println
-<<<<<<< HEAD
-		("Steve says Hello from the Master");}
-=======
-		("Steve says Hello from Branch AAAA");}
->>>>>>> branchA
+		("Steve says Hello from the Master and Branch A");}
 }
