918b3ef8ff909ef4793862c9b30f098da262abe8
Steve Harper <sharper@carroll.edu>
2018-10-11 12:00:34 -0600

    Added main class

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,8 +3,5 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		System.out.println
-
-
-		("United stuffwe stand, divided we fallme isSteve Hello Git Test in CLOUD");}
-
+		("Steve says Hello Git Test");}
 }
