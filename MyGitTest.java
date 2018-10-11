23bdfbb542f15500e51d989707072ea76ec70bed
Steve Harper <sharper@carroll.edu>
2018-10-11 13:10:52 -0600

    now

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,10 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		System.out.println
-<<<<<<< HEAD
-		("Blue and Green s;uy");
-=======
-		("Green CHANGEDs;uy");}
->>>>>>> b2b02153a528c0ec815a10cdaa555806bf54636d
-}
+
+		("Partners Blue and Green s;uy");
+	}
 }
